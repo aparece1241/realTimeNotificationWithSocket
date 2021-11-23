d0c520f54a598bf598c88e7b8745008d927656a1
aparece1241 <apareceh7@gmail.com>
2021-11-23 18:13:52 +0800

    Hot fixes on port

--- server.js
+++ server.js
@@ -6,34 +6,13 @@ const socket = require("socket.io");
 const port = process.env.PORT || 5000;
 
 const io = socket(server, {
-  cors: "*",
+  cors: {
+      origin: '*'
+  },
 });
 const cors = require("cors");
 
 app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, PATCH, DELETE, OPTIONS"
-    );
-    res.header("Access-Control-Max-Age", 120);
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
-app.get('/', (req, res) => {
-    res.send("App is running");
-})
 
 io.on("connection", (client) => {
   console.log("a client is connected!");
