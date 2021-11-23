81d5acc03013bba00f290313d4d7a7d670a30b91
aparece1241 <apareceh7@gmail.com>
2021-11-23 17:51:05 +0800

    add cors policy

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const socket = require('socket.io')
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const socket = require("socket.io");
 
-const port = process.env.port || 5000
+const port = process.env.port || 5000;
 
 const io = socket(server, {
-    cors: '*'
-})
-const cors = require('cors')
+  cors: "*",
+});
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-io.on('connection', client => {
-    console.log('a client is connected!')
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    client.on('notification', function(message) {
-        io.emit('notification', message)
-    })
-})
+  // Add this
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Max-Age", 120);
+    return res.status(200).json({});
+  }
 
-server.listen(port, serve => console.log('listening on port 5000'))
+  next();
+});
+
+io.on("connection", (client) => {
+  console.log("a client is connected!");
+
+  client.on("notification", function (message) {
+    io.emit("notification", message);
+  });
+});
+
+server.listen(port, (serve) => console.log("listening on port 5000"));
