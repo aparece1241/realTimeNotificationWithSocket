2c04e9991877de074be79b5d29a223c2a8881169
harvey <a-harvey@chronostep.com>
2022-06-13 11:55:32 +0800

    add udp server

--- server.js
+++ server.js
@@ -11,16 +11,38 @@ const io = socket(server, {
       methods: ["GET", "POST"]
   },
 });
+
+let udpSocket = 0
+
 const cors = require("cors");
 
 app.use(cors());
 
 io.on("connection", (client) => {
   console.log("a client is connected!");
-
+  
+  udpSocket = client
+  
   client.on("notification", function (message) {
     io.emit("notification", message);
   });
 });
 
 server.listen(port, (serve) => console.log("listening on port 5000"));
+
+
+
+//udp server on 41181
+var dgram = require("dgram");
+var server1 = dgram.createSocket("udp4");
+server1.on("message", function (msg, rinfo) {
+  console.log("msg: " + msg);
+  if (udpSocket != 0){
+    udpSocket.emit('field', ""+msg);
+  }
+});
+server1.on("listening", function () {
+  var address = server1.address();
+  console.log("udp server1 listening " + address.address + ":" + address.port);
+});
+server1.bind(41181);
